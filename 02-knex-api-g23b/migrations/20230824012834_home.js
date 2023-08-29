1d9161a61cef8e811d934a2e13a9f41d7ce4bdf6
César Guerra <warderer@gmail.com>
2023-08-28 20:45:19 -0600

    🚨 Format to Standard JS

--- 02-knex-api-g23b/migrations/20230824012834_home.js
+++ 02-knex-api-g23b/migrations/20230824012834_home.js
@@ -2,36 +2,36 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-    return knex.schema.hasTable('homes').then(function (exists) {
-        if (!exists) {
-          /* Si la tabla no existe, voy a crear una nueva tabla */
-          return knex.schema.createTable('homes', function (table) {
-            // sintaxis: table.tipoDeDato("nombreAtributo").chainableMethods
-            table.increments('house_id').primary()// en knex usamos increments en vez de serial como tipo de dato
-            table.string('title').notNullable() // notNullable es que no puede quedar nulo (not null)
-            table.text('description')
-            table.integer('guest')
-            table.text('address')
-            table.decimal('rental_price', 8, 2)
-            table.boolean('active').notNullable().defaultTo(true) // defaultTo: Especifico un valor por defecto
-            table.timestamp('created_at').defaultTo(knex.fn.now()) // knex.fn.now() me devuelve la fecha y hora actual al momento se crear el registro en la base de datos.
-          })
-        }
+exports.up = function (knex) {
+  return knex.schema.hasTable('homes').then(function (exists) {
+    if (!exists) {
+      /* Si la tabla no existe, voy a crear una nueva tabla */
+      return knex.schema.createTable('homes', function (table) {
+        // sintaxis: table.tipoDeDato("nombreAtributo").chainableMethods
+        table.increments('house_id').primary()// en knex usamos increments en vez de serial como tipo de dato
+        table.string('title').notNullable() // notNullable es que no puede quedar nulo (not null)
+        table.text('description')
+        table.integer('guest')
+        table.text('address')
+        table.decimal('rental_price', 8, 2)
+        table.boolean('active').notNullable().defaultTo(true) // defaultTo: Especifico un valor por defecto
+        table.timestamp('created_at').defaultTo(knex.fn.now()) // knex.fn.now() me devuelve la fecha y hora actual al momento se crear el registro en la base de datos.
       })
-};
+    }
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-    return knex.schema.hasTable("homes").then(function (exists) {
-        if (exists) {
-            return knex.schema.dropTable("homes");
-        }
-    })
-};
+exports.down = function (knex) {
+  return knex.schema.hasTable('homes').then(function (exists) {
+    if (exists) {
+      return knex.schema.dropTable('homes')
+    }
+  })
+}
 
 /* *** GUIA DE USO RÁPIDO DE KNEX */
 
--- 02-knex-api-g23b/migrations/20230824023845_home_has_user.js
+++ 02-knex-api-g23b/migrations/20230824023845_home_has_user.js
@@ -2,28 +2,28 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-    return knex.schema.hasTable('homes').then(function (exists) {
-        if (exists) {
-          return knex.schema.table('homes', function (table) {
-            table.integer('fk_user')
-                .unsigned()
-                .references('users.user_id')
-          })
-        }
+exports.up = function (knex) {
+  return knex.schema.hasTable('homes').then(function (exists) {
+    if (exists) {
+      return knex.schema.table('homes', function (table) {
+        table.integer('fk_user')
+          .unsigned()
+          .references('users.user_id')
       })
-};
+    }
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-    return knex.schema.hasTable("homes").then(function (exists) {
-        if (exists) {
-            return knex.schema.table("homes", function (table) {
-                table.dropColumn("fk_user");
-            });
-        }
-    })
-};
+exports.down = function (knex) {
+  return knex.schema.hasTable('homes').then(function (exists) {
+    if (exists) {
+      return knex.schema.table('homes', function (table) {
+        table.dropColumn('fk_user')
+      })
+    }
+  })
+}
